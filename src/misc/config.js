f7f6bc8085ce8d7d033200827d95d493f4ed1fa3
Yukesh_yuks <yukeshyukss@gmail.com>
2021-09-19 08:52:17 +0530

    added api fetch

---
+++ src/misc/config.js
@@ -0,0 +1,8 @@
+const API_URL = 'https://api.tvmaze.com';
+
+export async function apiGet(queryString) {
+
+  const response = await fetch(`${API_URL}${queryString}`).then(r => r.json());
+  return response;
+}
+
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,57 @@
-import React from 'react'
-import MainpageLayout from '../components/MainpageLayout'
+import React, { useState } from 'react';
+import MainpageLayout from '../components/MainpageLayout';
+import {apiGet} from "../misc/config"
 
 const Homepage = () => {
-    return <MainpageLayout>This is Home page</MainpageLayout>;
-  
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+
+  const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then(result =>{
+      setResults(result);
+      console.log(result);
+    });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0){
+      return <div>No Results</div> 
+    }
+    
+    if (results && results.length > 0 ){
+      return <div> {results.map( (item) => 
+      <div key={item.show.id}>{item.show.name}</div>
+      )}
+      </div>
+    };
+    return null;
 };
 
-export default Homepage
+  return (
+    <MainpageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainpageLayout>
+  );
+};
+
+export default Homepage;
