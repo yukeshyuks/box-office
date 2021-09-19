f2c7d2da6053690922a45e4671fab55a095cb6f8
Yukesh_yuks <yukeshyukss@gmail.com>
2021-09-19 10:39:14 +0530

    added separate search for movies and actors

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,9 +5,12 @@ import {apiGet} from "../misc/config"
 const Homepage = () => {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowsSearch = searchOption === 'shows';
 
   const onSearch = () => {
-    apiGet(`/search/shows?q=${input}`).then(result =>{
+    apiGet(`/search/${searchOption}?q=${input}`).then(result =>{
       setResults(result);
       console.log(result);
     });
@@ -29,22 +32,50 @@ const Homepage = () => {
     }
     
     if (results && results.length > 0 ){
-      return <div> {results.map( (item) => 
-      <div key={item.show.id}>{item.show.name}</div>
-      )}
-      </div>
+      return results[0].show ? results.map(item => 
+      <div key={item.show.id}>{item.show.name}</div>) :
+      results.map( item => (
+      <div key={item.person.id}>{item.person.name}</div>
+      ));
     };
+
     return null;
 };
 
+  const onRadioChange = (ev) => {
+    setSearchOption(ev.target.value);
+  }
+  
   return (
     <MainpageLayout>
       <input
         type="text"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
+        placeholder="Search for an movie/actor"
         value={input}
       />
+
+      <div>
+        <label htmlFor="movies-search">
+          Shows
+          <input id="movies-search" 
+            type="radio"
+            value="shows" 
+            onChange={onRadioChange}
+            checked={isShowsSearch}/>
+        </label>
+
+        <label htmlFor="actors-search">
+          Actors
+          <input id="actors-search" 
+            type="radio" 
+            value="people"
+            onChange={onRadioChange} 
+            checked={!isShowsSearch} />
+        </label>
+      </div>
+
 
       <button type="button" onClick={onSearch}>
         Search
