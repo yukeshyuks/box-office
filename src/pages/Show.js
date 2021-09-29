49588bbe038c7fc3461137e33b1e0ea1f74d0b06
Yukesh_yuks <yukeshyukss@gmail.com>
2021-09-29 07:26:19 +0530

    added show page and filled it with API data

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
+import Details from '../components/show/Details';
+import Season from '../components/show/Season';
+import Cast from '../components/show/Cast';
 
 const reducer = (prevState, action) => {
   switch (action.type) {
@@ -61,7 +66,38 @@ const Show = () => {
     return <div>Error occured: {error}</div>;
   }
 
-  return <div>this is show page</div>;
+  return <div>
+    <ShowMainData
+      image={show.image}
+      name={show.name}
+      rating={show.rating}
+      summary={show.summary}
+      tags={show.genres}
+    />
+
+    <div>
+      <h2>Details</h2>
+      <Details 
+        status={show.status}
+        network={show.network}
+        premiered={show.premiered}
+      />
+    </div>
+
+    <div>
+      <h2>Seasons</h2>
+      <Season 
+        seasons={show._embedded.seasons}
+      />
+    </div>
+
+    <div>
+      <h2>Cast</h2>
+      <Cast 
+        cast={show._embedded.cast}
+      />
+    </div>
+  </div>;
 };
 
 export default Show;
